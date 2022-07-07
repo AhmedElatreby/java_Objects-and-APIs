1e4260dede0499f49d7088188374a8a9a94cd08e
ahmedsport <ahmedsport@hotmail.com>
2022-07-07 18:07:46 +0100

    ic

--- Ch01/01_01/start/java_essentials/src/inheritance/people/Employee.java
+++ Ch01/01_01/start/java_essentials/src/inheritance/people/Employee.java
@@ -1,4 +1,22 @@
 package inheritance.people;
 
-public class Employee {
+public class Employee extends Person {
+    private String employeedId;
+    private String title;
+
+    public String getEmployeedId() {
+        return employeedId;
+    }
+
+    public void setEmployeedId(String employeedId) {
+        this.employeedId = employeedId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
