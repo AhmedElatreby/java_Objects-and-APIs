eeeee988ad77e3adeb793f5a6439da23688c786a
ahmedsport <ahmedsport@hotmail.com>
2022-07-09 15:07:34 +0100

    typecasting

--- Ch02/02_01/start/java_essentials/src/polymorphism/Zoo.java
+++ Ch02/02_01/start/java_essentials/src/polymorphism/Zoo.java
@@ -3,6 +3,24 @@ package polymorphism;
 public class Zoo {
 
     public static void main(String[] args) {
+        Dog molley = new Dog();
+        molley.fetch();
+        molley.makeSound();
+
+        Cat catly = new Cat();
+        catly.makeSound();
+        catly.scratch();
+
+        Animal catlly = new Cat();
+        catlly.makeSound();
+
+        Animal snowy = new Cat();
+        snowy.makeSound();
+        // typecasting
+        ((Cat)snowy).scratch();
+
+        snowy = new Dog();
+        snowy.makeSound();
 
     }
 }
