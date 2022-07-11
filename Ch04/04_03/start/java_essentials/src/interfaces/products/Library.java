1b6f2bc29a288e597f8b7cc88485dd9b971dbaee
ahmedsport <ahmedsport@hotmail.com>
2022-07-11 21:18:19 +0100

    implementing interfaces

--- Ch04/04_03/start/java_essentials/src/interfaces/products/Library.java
+++ Ch04/04_03/start/java_essentials/src/interfaces/products/Library.java
@@ -3,6 +3,8 @@ package interfaces.products;
 public class Library {
 
     public static void main(String[] args) {
+        Product book = new Book();
+        book.setName("The kite runner");
 
 
     }
--- Ch04/04_04/start/java_essentials/src/interfaces/products/Product.java
+++ Ch04/04_04/start/java_essentials/src/interfaces/products/Product.java
@@ -3,5 +3,13 @@ package interfaces.products;
 public interface Product {
 
     String getName();
+
     void setName(String name);
+
+    default double getPrice() {
+        return 50;
+    }
+
+    default void setPrice(double price) {
+    }
 }
