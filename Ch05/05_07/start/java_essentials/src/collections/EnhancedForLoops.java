aaf186d1028c6df398b29460695542afa3e6b048
ahmedsport <ahmedsport@hotmail.com>
2022-07-11 22:44:59 +0100

    Enhanced for loop

--- Ch05/05_07/start/java_essentials/src/collections/EnhancedForLoops.java
+++ Ch05/05_07/start/java_essentials/src/collections/EnhancedForLoops.java
@@ -6,10 +6,14 @@ import java.util.List;
 public class EnhancedForLoops {
 
     public static void main(String[] args) {
-        List fruits = new ArrayList();
+        List<String> fruits = new ArrayList<>();
         fruits.add("apple");
         fruits.add("lemon");
         fruits.add("banana");
         fruits.add("orange");
+
+        for ( String fruit: fruits ) {
+            System.out.println(fruit);
+        }
     }
 }
