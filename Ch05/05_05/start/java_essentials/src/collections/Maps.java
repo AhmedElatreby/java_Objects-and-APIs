9102153966146b0ddac50a0b439f531a511a3e1e
ahmedsport <ahmedsport@hotmail.com>
2022-07-11 22:30:51 +0100

    Maps

--- Ch05/05_05/start/java_essentials/src/collections/Maps.java
+++ Ch05/05_05/start/java_essentials/src/collections/Maps.java
@@ -13,6 +13,26 @@ public class Maps {
         fruitCalories.put("orange", 45);
 
         System.out.println(fruitCalories);
+        // to overrides existing value you can use put
+       // fruitCalories.put("apple", 100);
+        fruitCalories.putIfAbsent("apple", 100); // this will update the value if is not existed
+        System.out.println(fruitCalories);
+
+        // you can access the value of the item by .get method
+        System.out.println("lemon: " + fruitCalories.get("lemon") + " calories");
+
+        // to remove an item from the mep we use remove
+        fruitCalories.remove("lemon");
+        System.out.println(fruitCalories);
+
+        // you can make the map immutable by using Map.of
+        Map moreFruits = Map.of(
+                "apple", 100,
+                "lemon", 20
+        );
+        System.out.println(moreFruits);
+       // moreFruits.remove("apple"); // you cannot remove from immutable map
+
 
     }
 }
