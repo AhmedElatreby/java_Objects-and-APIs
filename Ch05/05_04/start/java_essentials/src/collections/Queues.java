8c4c6e05a92f8cefb807b9c20c0cc30917d31c1e
ahmedsport <ahmedsport@hotmail.com>
2022-07-11 22:12:27 +0100

    Queues

--- Ch05/05_04/start/java_essentials/src/collections/Queues.java
+++ Ch05/05_04/start/java_essentials/src/collections/Queues.java
@@ -6,6 +6,20 @@ import java.util.Queue;
 public class Queues {
 
     public static void main(String[] args) {
-        Queue fruits;
+        Queue fruits = new LinkedList<>();
+        fruits.add("apple");
+        fruits.add("lemon");
+        fruits.add("banana");
+        fruits.add("apple"); // it can hold duplicate items
+
+        System.out.println(fruits);
+
+        var removed = fruits.remove(); // you can store removed item into variable. remove only remove the first item of the list
+        System.out.println("removed item is: " + removed);
+
+        // the first item in the queues is called the head.
+        // you can view the head before remove it by using peek() method
+        System.out.println("Head of the queue is: " + fruits.peek());
+
     }
 }
