ee33b7976f6f8f3425bed94a3718e4250b95afbb
ahmedsport <ahmedsport@hotmail.com>
2022-07-11 21:59:41 +0100

    list

--- Ch05/05_03/start/java_essentials/src/collections/Lists.java
+++ Ch05/05_03/start/java_essentials/src/collections/Lists.java
@@ -7,6 +7,29 @@ public class Lists {
 
     public static void main(String[] args) {
 
-        List fruits;
+        List fruits = new ArrayList();
+        fruits.add("apple");
+        fruits.add("lemon");
+        fruits.add("banana");
+        fruits.add("orange");
+
+        System.out.println(fruits);
+
+        // you can use get to call the place of the item in the array
+        System.out.println(fruits.get(3));
+
+        // you can replace the item in the array by using set
+        fruits.set(3, "cherry");
+        fruits.add("lemon"); // you can add duplicate items
+
+        System.out.println(fruits);
+
+        // you can get the index of the item in the array
+        System.out.println(" index of first lemon: " + fruits.indexOf("lemon"));
+        System.out.println(" index of last lemon: " + fruits.lastIndexOf("lemon"));
+
+        // to remove from the array you can use remove
+        fruits.remove(4);
+        System.out.println(fruits);
     }
 }
