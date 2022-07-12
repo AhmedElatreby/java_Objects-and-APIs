1e05194c622d673c61cca33668334046f0952128
ahmedsport <ahmedsport@hotmail.com>
2022-07-12 11:10:46 +0100

    Try and catch

--- Ch07/07_01/start/java_essentials/src/exceptions/TryCatch.java
+++ Ch07/07_01/start/java_essentials/src/exceptions/TryCatch.java
@@ -6,6 +6,12 @@ import java.io.IOException;
 public class TryCatch {
 
     public static void main(String[] args) {
-        
+        File file = new File("a/file.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Sorry, unable to create file");
+        }
+
     }
 }
